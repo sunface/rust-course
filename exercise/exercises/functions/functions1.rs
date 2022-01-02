870c198982f33e42e8238288aaa8d4557a3e5fdd
unknown <huangchao@biteanbox.com>
2022-01-02 10:40:59 +0800

    save

--- exercise/exercises/functions/functions1.rs
+++ exercise/exercises/functions/functions1.rs
@@ -1,8 +1,9 @@
 // functions1.rs
-// Make me compile! Execute `rustlings hint functions1` for hints :)
+// 让我能够编译！执行 `rustex hint functions1` 获取提示 :)
 
 // I AM NOT DONE
 
+/// 翻译: [mg-chao](https://github.com/mg-chao)
 fn main() {
     call_me();
 }
