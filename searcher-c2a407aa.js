ac0f47461b8296fcd3335678162e5ceab3538c70
sunface <sunface@users.noreply.github.com>
2026-03-12 09:29:12 +0000

    deploy: 0cc482cb330cffec75fb0b47f8e4e7300c13f333

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-848c21ce.js',
+                path_to_root + 'searchindex-683616e6.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
